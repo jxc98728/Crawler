08b240584ef05201f1d4a0835ac8096308ab53f4
骁翀 蒋 <jxc98728@qq.com>
2018-12-03 17:31:24 +0800

    考虑建立一个Vector类型的线程共享 Question Pool
    当池非空时，某个线程从中取出某个问题id进行收录
    操作，搜索完成后将id加入到已完成队列中。要检测
    查到的相关问题id是否在已完成队列中，若不在，则
    添加到池中等待线程取走。通过设置完成队列的容量
    上限控制爬虫的运行时间。

--- src/Main.java
+++ src/Main.java
@@ -1,58 +1,68 @@
 import java.io.IOException;
+import java.util.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.select.Elements;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        Search s = new Search(304118539, 20);
+    }
+}
+
+class Search {
+    private HashSet<Integer> set;
+    private Hashtable<Integer, Integer> Set;
+
     private static String url_pattern = "https://www.zhihu.com/question/";
     private static String similar_url_pattern1 = "https://www.zhihu.com/api/v4/questions/";
     private static String similar_url_pattern2 = "/similar-questions?include=data%5B*%5D.answer_count%2Cauthor%2Cfollower_count&limit=5";
     private static String selector_pattern1 = "#QuestionAnswers-answers > div > div > div:nth-child(2) > div > div:nth-child(";
     private static String selector_pattern2 = ") > div > div.RichContent.RichContent--unescapable > div.RichContent-inner";
+    private static String question_selector = "#root > div > main > div > div:nth-child(11) > div.QuestionHeader > div.QuestionHeader-content > div.QuestionHeader-main > h1";
 
-    public static List<String> getSubUtil(String str, String regex) {
-        List<String> list = new ArrayList<String>();
-        Pattern pattern = Pattern.compile(regex);
+    public Search(int q_id, int recursion_level) throws IOException {
+        set = new HashSet<>();
+        Set = new Hashtable<>();
+        recursive_search(q_id, recursion_level);
+    }
+
+
+    private static List<Integer> getSubUtil(String str) {
+        List<Integer> list = new ArrayList<Integer>();
+        Pattern pattern = Pattern.compile("\"id\":(.*?),\"title\"");
         Matcher m = pattern.matcher(str);
         while (m.find()) {
-            list.add(m.group(1));
+            list.add(Integer.parseInt(m.group(1)));
         }
         return list;
     }
 
+    private void recursive_search(int q_id, int recursion_level) throws IOException {
+        if (recursion_level == 0)
+            return;
+        if (set.contains(q_id))
+            return;
+        set.add(q_id);
+        String url = url_pattern + q_id;
+        String similar_url = similar_url_pattern1 + q_id + similar_url_pattern2;
 
-    public static void main(String[] args) throws IOException {
-        String url_ID = args[0];
-        String url = url_pattern + url_ID;
         Document doc = Jsoup.connect(url).get();
-        Elements[] answer = new Elements[5];
-
-        String similar_url = similar_url_pattern1 + url_ID + similar_url_pattern2;
         Document similar_doc = Jsoup.connect(similar_url).ignoreContentType(true).get();
+
         String similar_data = similar_doc.text();
-        List<String> similar_ID = getSubUtil(similar_data, "\"id\":(.*?),\"title\"");
+        List<Integer> similar_ID = getSubUtil(similar_data);
 
-        // parse selector
-        String[] selector = new String[5];
+        // Print the question
+        System.out.println(doc.select(question_selector).text());
 
-        // Print the content
-        for (int i = 0; i < 5; ++i) {
-            selector[i] = selector_pattern1 + (i + 1) + selector_pattern2;
-            answer[i] = doc.select(selector[i]);
-            if (answer[i].hasText())
-                System.out.println(answer[i].text());
-        }
-
-        // Print the related question ID
-        for (int i = 0; i < similar_ID.size(); ++i) {
-            System.out.println(similar_ID.get(i));
+        // Recursive search
+        for (int similar_id : similar_ID) {
+            recursive_search(similar_id, recursion_level - 1);
         }
     }
-
 }
---
+++ src/MultithreadSearch.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+public class MultithreadSearch {
+    public static void main(String[] args) {
+
+    }
+
+}
